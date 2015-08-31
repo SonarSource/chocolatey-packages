8d990f35a302563b8c02036182bee2425a6633bf
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2015-08-31 14:00:15 +0200

    Add MSBuild SonarQube Runner 1.0 - Fixes #1

---
+++ msbuild-sonarqube-runner/tools/chocolateyinstall.ps1
@@ -0,0 +1,5 @@
+﻿Install-ChocolateyZipPackage 'msbuild-sonarqube-runner' `
+  -url 'https://github.com/SonarSource/sonar-msbuild-runner/releases/download/1.0/MSBuild.SonarQube.Runner-1.0.zip' `
+  -unzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" `
+  -checksumType 'sha1' `
+  -checksum '65FF67D99F357CF7667AC9B9A4DD6B46918AD217'
---
+++ msbuild-sonarqube-runner/tools/chocolateyuninstall.ps1
@@ -0,0 +1,1 @@
+﻿UnInstall-ChocolateyZipPackage 'msbuild-sonarqube-runner' 'MSBuild.SonarQube.Runner-1.0.zip'
