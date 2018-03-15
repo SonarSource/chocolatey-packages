6efe46c7843548141df3dfc7281613ceff4927cf
michalb-sonar <michal.barczyk@sonarsource.com>
2018-03-15 17:00:55 +0100

    Update Scanner for MSBuild version to 4.1 (#18)

--- msbuild-sonarqube-runner/tools/chocolateyinstall.ps1
+++ msbuild-sonarqube-runner/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'msbuild-sonarqube-runner'
-$url = 'https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/4.0.2.892/sonar-scanner-msbuild-4.0.2.892.zip'
-$checksum = '1d8829ab54fb78f396849033edd5e96230f28f00'
+$url = 'https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/4.1.0.1148/sonar-scanner-msbuild-4.1.0.1148-net46.zip'
+$checksum = '5bb027d5a77fa91d7a9aeada6b0c31d1314d1688'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Install-ChocolateyZipPackage $packageName `
   -url "$url" `
